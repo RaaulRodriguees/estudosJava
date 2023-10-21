6d7d84b1f46c50ecac2feace3af6a05e3acfd44d
Raaaaaul <107852198+RaaulRodriguees@users.noreply.github.com>
2023-10-20 22:04:32 -0300

    Update and rename Exercicio2.java to ConcatenacaoOperacoesMath.java

--- ConcatenacaoOperacoesMath.java
+++ ConcatenacaoOperacoesMath.java
@@ -1,28 +1,40 @@
-package lista_dalilo;
-
 import java.util.Scanner;
 
 public class Exercicio2 {
     public static void main(String[]args){
+        
+        //Cria um Scanner chamado sc
+        //Cria um Scanner chamado sc
         Scanner sc = new Scanner(System.in);
 
+        //Usuário insere o primeiro valor
+        //User enters the first value
         System.out.print("Escreva o primeiro valor: ");
         int num1 = sc.nextInt();
 
+        //Usuário insere o segundo valor
+        //User enters the second value
         System.out.print("Escreva o segundo valor: ");
         int num2 = sc.nextInt();
 
+        //Cria variáveis para as operações matematicas
+        //Create variables for mathematical operations
         int soma = num1 + num2;
         int sub = num1 - num2;
         int div = num1/num2;
         int multi = num1 * num2;
-
+        
         System.out.println();
+        
+        //Faz as operações e depois concatena com o sysout
+        //Perform the operations and then concatenate with the sysout
         System.out.println("A soma dos valores é: " + soma);
         System.out.println("A subtração dos valores é: " + sub);
         System.out.println("A multiplicação dos valores é: " + multi);
         System.out.println("A divisão dos valores é: " + div);
 
+        //Fecha o Scanner
+        //Close the Scanner
         sc.close();
     }
 }
